f1d8d4acf814ae319a9b8b0ce4a8267f598ff450
James Robert <jim.mixtake@gmail.com>
2011-12-05 13:25:41 -0500

    increment version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name='django-shpaml',
-    version='1.0.0',
+    version='1.0.1',
     author='James Robert',
     description=('A Django template loader for loading and converting '
                  'SHPAML markup to HTML'),
