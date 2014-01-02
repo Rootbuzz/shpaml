2bdf5cde53ef8661842d3a739f092c8716000739
James Robert <jiaaro@gmail.com>
2014-01-02 13:56:29 -0500

    change package to mention location of stand-along shpaml implementation since the original website is no longer running

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 __doc__ = """
 A Django template loader for loading and converting SHPAML markup to HTML
 
-The django SHPAML template loader uses the official Python SHPAML implementation
-which can be found at http://shpaml.webfactional.com/
-
-Note: the SHPAML implementation from the above link is included for your
-convenience.
+Note: A SHPAML implementation is included for your
+convenience. If you are interested in using shpaml alone, please see
+shpaml/shpaml.py
 """
 
 from setuptools import setup
