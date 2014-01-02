6f75a8c00f37356638f2d63fd2a5df4b183430ad
Ram Rachum <ram@rachum.com>
2014-01-02 21:15:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
