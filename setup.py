80791031f5b129b6f0869e2d6b88aed4919505a5
James Robert <jiaaro@gmail.com>
2014-02-02 13:58:48 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name='django-shpaml',
-    version='1.1.0',
+    version='1.2.0',
     author='James Robert',
     author_email='shpaml@jiaaro.com',
     description=('A Django template loader for loading and converting '
